e9857e645e0d546646ae326cf4f2b8beae78935f
Ruben Vermeulen <ruben.vermeulen@hotmail.com>
2016-05-10 21:16:52 +0200

    Initialiseren van project

---
+++ JavaWebProjecten2/src/main/java/config/WebConfig.java
@@ -0,0 +1,35 @@
+package config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("controller")
+@Import({PersistenceJPAConfig.class})
+public class WebConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("resources/css/");
+        registry.addResourceHandler("/img/**").addResourceLocations("resources/img/");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+
+        InternalResourceViewResolver resolver
+                = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+}
